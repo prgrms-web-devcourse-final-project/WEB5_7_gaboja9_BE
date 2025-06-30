f7d11cd6b029869ad2dc48a76b60456c2f491f3f
Kim, Seokwan <sukwan1106@gmail.com>
2025-06-30 15:47:59 +0900

    Chore: 프로젝트 초기 설정

---
+++ MockStock/src/main/java/io/gaboja9/mockstock/MockStockApplication.java
@@ -0,0 +1,13 @@
+package io.gaboja9.mockstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockStockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MockStockApplication.class, args);
+    }
+
+}
---
+++ MockStock/src/test/java/io/gaboja9/mockstock/MockStockApplicationTests.java
@@ -0,0 +1,13 @@
+package io.gaboja9.mockstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockStockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
