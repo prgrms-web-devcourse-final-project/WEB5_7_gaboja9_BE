b4fc57c68757ed7603eeed25146e413e257447e6
ksw733 <129919107+ksw733@users.noreply.github.com>
2025-07-14 11:01:58 +0900

    fix: 카카오페이 관련 스웨거 추가 (#44)
    * feat: 카카오페이 관련 스웨거 추가
    
    * chore: Java 스타일 수정
    
    ---------
    
    Co-authored-by: github-actions <>

--- MockStock/src/main/java/io/gaboja9/mockstock/domain/payments/dto/PaymentRequest.java
+++ MockStock/src/main/java/io/gaboja9/mockstock/domain/payments/dto/PaymentRequest.java
@@ -1,5 +1,9 @@
 package io.gaboja9.mockstock.domain.payments.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import jakarta.validation.constraints.NotBlank;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,7 +11,10 @@ import lombok.NoArgsConstructor;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@Schema(description = "결제 요청")
 public class PaymentRequest {
 
+    @Schema(description = "충전 금액", example = "10000", required = true)
+    @NotBlank
     private int chargeAmount;
 }
