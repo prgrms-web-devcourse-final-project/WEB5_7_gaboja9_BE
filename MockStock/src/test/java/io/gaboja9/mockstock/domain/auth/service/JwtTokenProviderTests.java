1775be6755123493a05ee3144e277920228e03ce
dbogym <ko07073@gmail.com>
2025-07-16 02:47:59 +0900

    chore: test yml 파일 수정 (OAuth2, JWT 관련 추가)

--- MockStock/src/test/java/io/gaboja9/mockstock/domain/auth/service/JwtTokenProviderTests.java
+++ MockStock/src/test/java/io/gaboja9/mockstock/domain/auth/service/JwtTokenProviderTests.java
@@ -1,30 +1,30 @@
-// package io.gaboja9.mockstock.domain.auth.service;
-//
-// import io.gaboja9.mockstock.domain.members.enums.Role;
-// import lombok.extern.slf4j.Slf4j;
-// import org.junit.jupiter.api.BeforeEach;
-// import org.junit.jupiter.api.DisplayName;
-// import org.junit.jupiter.api.Test;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.boot.test.context.SpringBootTest;
-//
-// import static org.junit.jupiter.api.Assertions.*;
-//
-// @Slf4j
-// @SpringBootTest
-// class JwtTokenProviderTests {
-//
-//    @Autowired
-//    JwtTokenProvider provider;
-//
-//    @Test
-//    @DisplayName("JWT 발급 테스트")
-//    void jwt_issue_test() throws Exception{
-//
-//        Long targetId = 1L;
-//        Role targetRole = Role.MEMBER;
-//
-//        String token = provider.issueAcceessToken(targetId, targetRole);
-//        log.info("token = {}", token);
-//    }
-// }
+ package io.gaboja9.mockstock.domain.auth.service;
+
+ import io.gaboja9.mockstock.domain.members.enums.Role;
+ import lombok.extern.slf4j.Slf4j;
+ import org.junit.jupiter.api.BeforeEach;
+ import org.junit.jupiter.api.DisplayName;
+ import org.junit.jupiter.api.Test;
+ import org.springframework.beans.factory.annotation.Autowired;
+ import org.springframework.boot.test.context.SpringBootTest;
+
+ import static org.junit.jupiter.api.Assertions.*;
+
+ @Slf4j
+ @SpringBootTest
+ class JwtTokenProviderTests {
+
+    @Autowired
+    JwtTokenProvider provider;
+
+    @Test
+    @DisplayName("JWT 발급 테스트")
+    void jwt_issue_test() throws Exception{
+
+        Long targetId = 1L;
+        Role targetRole = Role.MEMBER;
+
+        String token = provider.issueAcceessToken(targetId, targetRole);
+        log.info("token = {}", token);
+    }
+ }
