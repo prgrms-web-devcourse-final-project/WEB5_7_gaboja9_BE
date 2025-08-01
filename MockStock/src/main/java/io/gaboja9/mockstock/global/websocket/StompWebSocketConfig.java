8f51e254e7caadd14c397bf9580bb57abd4ba26c
hs986 <94986211+hs986@users.noreply.github.com>
2025-08-01 10:34:22 +0900

    Update StompWebSocketConfig.java
    .

--- MockStock/src/main/java/io/gaboja9/mockstock/global/websocket/StompWebSocketConfig.java
+++ MockStock/src/main/java/io/gaboja9/mockstock/global/websocket/StompWebSocketConfig.java
@@ -45,7 +45,7 @@ public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
                         "http://127.0.0.1:5500",
                         "http://localhost:5173",
                         "https://localhost:5173",
-                        "http://127.0.0.1:5500",
+                        "https://127.0.0.1:5173",
                         "https://127.0.0.1:5500")
                 .withSockJS();
 
